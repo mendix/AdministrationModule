6267a8ba3eaa6372b5e7f89276b1979cb85e51b4
Allard Brand <allard.brand@mendix.com>
2023-04-13 16:44:32 +0200

    [UIA-433] Upgrade to Mx 9.18.0 + remove supporting microflows for Mendix SSO

--- README.md
+++ README.md
@@ -11,16 +11,6 @@ This module contains administration functionalities to manage local accounts and
 
 ## Dependencies
 - Atlas_Core package
-- Mendix SSO module
-
-## Mendix SSO
-If you want to use this module in combination with Mendix SSO, use the following steps:
-- Make sure that your project contains the MendixSSO module (v3.0.0 or higher). If you do not have the MendixSSO module in your project yet, import the module from the Marketplace (https://marketplace.mendix.com/link/component/111349)
-- Configure the MendixSSO_AfterStartup microflow (available in the 'Mendix SSO' folder) from the Administration module as the After startup microflow. If there is already an After startup microflow, do not replace it, but add the MendixSSO_AfterStartup microflow as a sub-microflow in the existing microflow.
-- If you previously used the Mendix SSO in your application and used MendixSSOUser as the user entity, please use the 'MendixSSO_MigrateUsersToAccount' microflow to migrate users from the MendixSSOUser to the Administration.Account specialization. Make sure to read the instructions in the microflow carefully before executing the migration.
-
-If you do not want to make use of Mendix SSO in your project, use the following steps:
-- Delete the 'Mendix SSO' folder in this module or exclude all its microflows.
 
 ## License
 See included [LICENSE](LICENSE) file
--- Source/javasource/administration/proxies/microflows/Microflows.java
+++ Source/javasource/administration/proxies/microflows/Microflows.java
@@ -7,13 +7,19 @@ package administration.proxies.microflows;
 import java.util.HashMap;
 import java.util.Map;
 import com.mendix.core.Core;
-import com.mendix.core.CoreException;
-import com.mendix.systemwideinterfaces.MendixRuntimeException;
 import com.mendix.systemwideinterfaces.core.IContext;
 import com.mendix.systemwideinterfaces.core.IMendixObject;
 
 public class Microflows
 {
+	/**
+	 * @deprecated
+	 * The default constructor of the Microflows class should not be used.
+	 * Use the static microflow invocation methods instead.
+	 */
+	@java.lang.Deprecated(since = "9.12", forRemoval = true)
+	public Microflows() {}
+
 	// These are the microflows for the Administration module
 	public static void changeMyPassword(IContext context, administration.proxies.AccountPasswordData _accountPasswordData)
 	{
@@ -31,33 +37,6 @@ public class Microflows
 	{
 		Map<java.lang.String, Object> params = new HashMap<>();
 		Core.microflowCall("Administration.ManageMyAccount").withParams(params).execute(context);
-	}
-	public static boolean mendixSSO_AfterStartup(IContext context)
-	{
-		Map<java.lang.String, Object> params = new HashMap<>();
-		return (java.lang.Boolean) Core.microflowCall("Administration.MendixSSO_AfterStartup").withParams(params).execute(context);
-	}
-	public static administration.proxies.Account mendixSSO_CreateUser(IContext context, mendixsso.proxies.UserProfile _userProfile, java.lang.String _uUID)
-	{
-		Map<java.lang.String, Object> params = new HashMap<>();
-		params.put("UserProfile", _userProfile == null ? null : _userProfile.getMendixObject());
-		params.put("UUID", _uUID);
-		IMendixObject result = (IMendixObject)Core.microflowCall("Administration.MendixSSO_CreateUser").withParams(params).execute(context);
-		return result == null ? null : administration.proxies.Account.initialize(context, result);
-	}
-	public static void mendixSSO_MigrateUsersToAccount(IContext context)
-	{
-		Map<java.lang.String, Object> params = new HashMap<>();
-		Core.microflowCall("Administration.MendixSSO_MigrateUsersToAccount").withParams(params).execute(context);
-	}
-	public static administration.proxies.Account mendixSSO_UpdateUser(IContext context, administration.proxies.Account _user, mendixsso.proxies.UserProfile _userProfile, java.lang.String _uUID)
-	{
-		Map<java.lang.String, Object> params = new HashMap<>();
-		params.put("User", _user == null ? null : _user.getMendixObject());
-		params.put("UserProfile", _userProfile == null ? null : _userProfile.getMendixObject());
-		params.put("UUID", _uUID);
-		IMendixObject result = (IMendixObject)Core.microflowCall("Administration.MendixSSO_UpdateUser").withParams(params).execute(context);
-		return result == null ? null : administration.proxies.Account.initialize(context, result);
 	}
 	public static void newAccount(IContext context)
 	{
@@ -77,14 +56,13 @@ public class Microflows
 	{
 		Map<java.lang.String, Object> params = new HashMap<>();
 		java.util.List<IMendixObject> objs = Core.microflowCall("Administration.RetrieveTimeZones").withParams(params).execute(context);
-		java.util.List<system.proxies.TimeZone> result = null;
-		if (objs != null)
-		{
-			result = new java.util.ArrayList<>();
-			for (IMendixObject obj : objs)
-				result.add(system.proxies.TimeZone.initialize(context, obj));
+		if (objs == null) {
+			return null;
+		} else {
+			return objs.stream()
+				.map(obj -> system.proxies.TimeZone.initialize(context, obj))
+				.collect(java.util.stream.Collectors.toList());
 		}
-		return result;
 	}
 	public static void saveNewAccount(IContext context, administration.proxies.AccountPasswordData _accountPasswordData)
 	{
